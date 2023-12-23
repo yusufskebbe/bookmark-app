3def4096e7aa3301a2694670941b9374cd5bc104
yusuf <kebbeyusuf@gmail.com>
2023-12-23 13:11:16 +0300

    form validation function implementation

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ function closeModal() {
   modal.classList.remove('show-modal')
 }
 
-// event listener
+
+
+// modal event listener
 modalShow.addEventListener('click', showModal)
 modalClose.addEventListener('click', closeModal)
 window.addEventListener('click', (e) => {
@@ -25,3 +27,42 @@ window.addEventListener('click', (e) => {
   e.target == modal ? modal.classList.remove('show-modal') : false
 
 })
+
+// validate form
+
+function validate(nameValue, urlValue) {
+
+  const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+  const regex = new RegExp(expression);
+
+  if (!nameValue || !urlValue) {
+    alert('please submit values for both fields')
+    return false
+  }
+  if (!urlValue.match(regex)) {
+    alert('please provide a valid web address')
+    return false
+  }
+  return true
+
+}
+
+
+// Handle Data 
+
+function storeBookmark(e) {
+  e.preventDefault()
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteUrlEl.value;
+  if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
+    urlValue = `https://${urlValue}`;
+  }
+  console.log(nameValue, urlValue);
+  if (!validate(nameValue, urlValue)) {
+    return false
+  }
+}
+
+// event listener 
+
+bookmarkForm.addEventListener('submit', storeBookmark)
